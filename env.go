dc1f3a5559eb40103ff1fe9d61de44d9266b7078
Chetan Narsude <chetan@celeral.com>
2023-09-02 17:56:53 -0700

    enabled support for mocking non exported fields

--- env.go
+++ env.go
@@ -3,12 +3,15 @@ package envygo
 import (
 	"fmt"
 	"reflect"
+	"unsafe"
 )
 
 var registered []interface{}
 
 // Introduce introduces (akin to registers) environments which are
 // likely to be mocked in future using MockMany function
+// Deprecated Introduce is being considered for deletion to
+// reduce the footprint of the API
 func Introduce(envs ...interface{}) {
 	registered = append(registered, envs...)
 }
@@ -18,6 +21,9 @@ func Introduce(envs ...interface{}) {
 // Each of the envs is matched to a previously introduced environment
 // based on type commonality and the introduced environment is
 // mocked using the passed environment.
+// Deprecated MockMany is being considered for deletion to remove
+// the linkage between Introduce and MockMany. It simplifies the
+// API by reducing complexity and footprint both at the same time.
 func MockMany(envs ...interface{}) func() {
 	var funcs []func()
 
@@ -25,7 +31,7 @@ func MockMany(envs ...interface{}) func() {
 		unknownType := reflect.ValueOf(unknown).Type()
 		for _, known := range registered {
 			if reflect.ValueOf(known).Type() == unknownType {
-				funcs = append(funcs, MockOne(known, reflect.ValueOf(unknown).Elem().Interface()))
+				funcs = append(funcs, Mock(known, unknown))
 				unknownType = nil
 			}
 		}
@@ -35,6 +41,11 @@ func MockMany(envs ...interface{}) func() {
 		}
 	}
 
+	return Unmock(funcs...)
+}
+
+// Unmock is handy to invoke the return values of Mock family of methods
+func Unmock(funcs ...func()) func() {
 	return func() {
 		for _, function := range funcs {
 			defer function()
@@ -42,48 +53,71 @@ func MockMany(envs ...interface{}) func() {
 	}
 }
 
-// MockOne mocks the old environment using the values in the new environment
+func getNonExportedField(field reflect.Value) reflect.Value {
+	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem()
+}
+
+type field struct {
+	name     string
+	value    interface{}
+	exported bool
+}
+
+func toPairs(new interface{}, includeZeros bool) []field {
+	var fields []field = nil
+
+	valueOfNew := reflect.ValueOf(new).Elem()
+	typeOfNew := reflect.TypeOf(new).Elem()
+
+	for i := typeOfNew.NumField(); i > 0; {
+		i--
+
+		newField := valueOfNew.Field(i)
+
+		var value interface{}
+
+		exported := typeOfNew.Field(i).IsExported()
+		if exported {
+			value = newField.Interface()
+		} else {
+			value = getNonExportedField(newField).Interface()
+		}
+
+		if includeZeros || !isZero(reflect.ValueOf(value)) {
+			fields = append(fields, field{typeOfNew.Field(i).Name, value, exported})
+		}
+	}
+
+	return fields
+}
+
+// Mock mocks the old environment using the values in the new environment
 // If the type for old environment is identical to the type of the new environment
 // then any attribute with value identical to default value for its type is
 // not mocked. But if types are different then the attribute is mocked regardless
 // of its value
-func MockOne(old interface{}, new interface{}) func() {
-	valueOfNew := reflect.ValueOf(new)
-	typeOfNew := reflect.TypeOf(new)
+func Mock(old interface{}, new interface{}) func() {
+	array := toPairs(new, reflect.TypeOf(new).Elem() != reflect.ValueOf(old).Elem().Type())
+	if array == nil {
+		return func() {}
+	}
 
-	blankPtrValue := reflect.New(typeOfNew)
-	valueOfBlank := blankPtrValue.Elem()
+	array = mockHelper(old, array)
+	return func() {
+		mockHelper(old, array)
+	}
+}
 
+func mockHelper(old interface{}, fields []field) []field {
 	oldPtrVal := reflect.New(reflect.TypeOf(old))
 	oldPtrVal.Elem().Set(reflect.ValueOf(old))
 	valueOfOld := oldPtrVal.Elem().Elem()
 
-	includeUnset := typeOfNew != reflect.ValueOf(old).Elem().Type()
-
-	for i := valueOfNew.NumField(); i > 0; {
-		i--
-		if typeOfNew.Field(i).IsExported() {
-			newField := valueOfNew.Field(i)
-			value := newField.Interface()
-			if includeUnset || !isZero(reflect.ValueOf(value)) {
-				name := typeOfNew.Field(i).Name
-				oldField := valueOfOld.FieldByName(name)
-				if oldField.CanSet() {
-					blankField := valueOfBlank.Field(i)
-					blankField.Set(oldField)
-					oldField.Set(newField)
-				} else {
-					panic(fmt.Sprintf("Attempt to mock unregistered field via %s", name))
-				}
-			}
-		}
+	for i, f := range fields {
+		fields[i].value = mockField(valueOfOld, f.name, f.value, f.exported)
 	}
 
-	blank := valueOfBlank.Interface()
-
-	return func() {
-		MockOne(old, blank)
-	}
+	return fields
 }
 
 func isZero(v reflect.Value) bool {
@@ -105,5 +139,56 @@ func isZero(v reflect.Value) bool {
 	}
 
 	// this takes care of the non-exported fields
-	return v.IsValid() && v.IsZero()
+	return !v.IsValid() || v.IsZero()
+}
+
+// MockField replaces the value of the field identified by `name` with `value`
+func MockField(old interface{}, name string, value any) func() {
+	typeOf := reflect.TypeOf(old).Elem()
+	if typeField, found := typeOf.FieldByName(name); found {
+		exported := typeField.IsExported()
+		valueOfStruct := reflect.ValueOf(old).Elem()
+		old := mockField(valueOfStruct, name, value, exported)
+		return func() {
+			mockField(valueOfStruct, name, old, exported)
+		}
+	}
+
+	panic("no property with name " + name + " found")
+}
+
+// Fields allows specifying multiple mappings by using field names.
+type Fields map[string]interface{}
+
+// MockFields mocks many fields of the struct pointed to by old
+func MockFields(old interface{}, fields Fields) func() {
+	typeOf := reflect.TypeOf(old).Elem()
+	valueOfStruct := reflect.ValueOf(old).Elem()
+
+	var array []field
+	for name, value := range fields {
+		if typeField, found := typeOf.FieldByName(name); found {
+			exported := typeField.IsExported()
+			old := mockField(valueOfStruct, name, value, exported)
+			array = append(array, field{name, old, exported})
+		}
+	}
+
+	if array == nil {
+		return func() {}
+	}
+
+	return func() {
+		mockHelper(old, array)
+	}
+}
+
+func mockField(valueOfStruct reflect.Value, name string, new any, exported bool) interface{} {
+	field := valueOfStruct.FieldByName(name)
+	if !exported {
+		field = getNonExportedField(field)
+	}
+	old := field.Interface()
+	field.Set(reflect.ValueOf(new))
+	return old
 }
--- env_test.go
+++ env_test.go
@@ -34,7 +34,7 @@ func TestEnvMockRandom(t *testing.T) {
 	}
 
 	url := "http://host:port/this/is/sample/path"
-	defer MockOne(&env, struct {
+	defer Mock(&env, &struct {
 		Create func(string) (*os.File, error)
 		Path   string
 		umask  int
@@ -66,7 +66,7 @@ func TestEnvMockSame(t *testing.T) {
 	}
 
 	url := "http://host:port/this/is/sample/path"
-	defer MockOne(&env, OsEnv{
+	defer Mock(&env, &OsEnv{
 		Create: func(s string) (*os.File, error) {
 			return nil, errors.New("cant create a file")
 		},
@@ -106,5 +106,67 @@ func TestMock(t *testing.T) {
 
 	if _, err := env.Create("hello"); err == nil {
 		t.Errorf("error was expected")
+	}
+}
+
+func TestUnexportedMocking(t *testing.T) {
+	var env = OsEnv{}
+
+	const perm = 0o022
+	defer Mock(&env, &struct {
+		Path  string
+		umask int
+	}{"Hello", perm})()
+
+	if perm != env.umask {
+		t.Fail()
+	}
+}
+
+func TestMockField(t *testing.T) {
+	var env = OsEnv{}
+
+	const perm = 0o022
+	defer MockField(&env, "umask", perm)()
+
+	if perm != env.umask {
+		t.Fail()
+	}
+}
+
+func TestMockFields(t *testing.T) {
+	var env = OsEnv{}
+
+	const perm = 0o022
+	const path = "path to my home"
+	defer MockFields(&env, Fields{"umask": perm, "Path": path})()
+
+	if perm != env.umask || path != env.Path {
+		t.Fail()
+	}
+}
+
+func helperResetMockFields(env interface{}, tester func()) {
+	const perm = 0o022
+	const path = "path to my home"
+	defer MockFields(env, Fields{"umask": perm, "Path": path})()
+	tester()
+}
+
+func TestResetMockFields(t *testing.T) {
+	var env = OsEnv{}
+	perm := env.umask
+	path := env.Path
+	var called bool
+	ptr := &called
+	helperResetMockFields(&env, func() {
+		*ptr = true
+		if perm == env.umask || path == env.Path {
+			t.Fail()
+		}
+	})
+
+	if !called || perm != env.umask || path != env.Path {
+		t.Fail()
 	}
 }
